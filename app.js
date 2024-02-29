245d89fac79c02c5820b64f6146d7a1585e1e0df
MuhammadMuiz <122803576+M-Muiz@users.noreply.github.com>
2024-02-29 23:06:34 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+// var a = 1;
+// var b = 2;
+// var c = 3;
+
+// var eq = c++ + c++ +b + b++ - --a + b-- + b + --c + c++ + c;
+
+// console.log(++c) fisrst plus 1 in value and then console the updated value
+// console.log(c++) plus 1 in value after this console
+
+
+// var firstName = prompt("Enter your first name");
+
+// var lastName = prompt("Enter your last name");
+
+// var result = firstName + lastName;
+
+// document.write("Your full name is " + result);
+
+
+// var a = 5;
+
+// // top to bottom or left to right
+
+// console.log(-- a)
